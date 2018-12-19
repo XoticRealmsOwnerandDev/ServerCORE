622a96a857754b94e2f634a4c68ce4fbd12631dc
XoticRealmsOwnerandDev <XoticRealmshelp@gmail.com>
2018-12-19 08:41:46 -0600

    Update Main.java

--- com/RoyalCraft/CORE/Main.java
+++ com/RoyalCraft/CORE/Main.java
@@ -9,12 +9,12 @@ public class Main extends Javaplugin; {
 
     @Override 
     public void onEnable() {
-        system.out.println("Core Enabled");
+        System.out.println("Core Enabled");
     }
 
     @Override
     public void OnDisable() {
-        system.out.println("Core Disabled");
+        System.out.println("Core Disabled");
     }
 
     public boolean onCommand(CommandSender Sender, Command cmd, String label, String[] args) {
@@ -23,9 +23,13 @@ public class Main extends Javaplugin; {
            if (sender instanceof Player) {
                Player player = (Player) sender;
                 
-               player.sendMessage("You Have Been Healed");
+               player.sendMessage(ChatColor.GREEN + "You Have Been Healed");
+               player.setHealth(100.0);
             } else {
-                system.out.println("You Cannot Use This Command In Console")
+                System.out.println("=======================")
+                System.out.println("This Command Can Only be Used In Game")
+                System.out.println("Do /heal In Game and it heal you")
+                System.out.println("=======================")
             }
 
         }
