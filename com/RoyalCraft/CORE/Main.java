d3dfa8e3685fb157de3d98a69c8ac8618e83daed
XoticRealmsOwnerandDev <XoticRealmshelp@gmail.com>
2018-12-19 08:06:49 -0600

    Update and rename Main.class to Main.java

---
+++ com/RoyalCraft/CORE/Main.java
@@ -0,0 +1,22 @@
+package com.RoyalCraft.CORE;
+
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.entity.player;
+
+public class Main extends Javaplugin; {
+
+    @Override 
+    public void onEnable() {
+        system.out.println("Core Enabled");
+}
+
+    @Override
+    public void OnDisable() {
+        system.out.println("Core Disabled");
+}
+
+
+}
+
+
+
