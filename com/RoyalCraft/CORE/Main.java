53b0e8b37be47e7a675c528773418d779451a349
XoticRealmsOwnerandDev <XoticRealmshelp@gmail.com>
2018-12-19 08:25:45 -0600

    Update Main.java

--- com/RoyalCraft/CORE/Main.java
+++ com/RoyalCraft/CORE/Main.java
@@ -2,19 +2,36 @@ package com.RoyalCraft.CORE;
 
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.entity.player;
+import org.bukkit.Command;
+import org.bukkit.CommandSender;
 
 public class Main extends Javaplugin; {
 
     @Override 
     public void onEnable() {
         system.out.println("Core Enabled");
-}
+    }
 
     @Override
     public void OnDisable() {
         system.out.println("Core Disabled");
-}
+    }
 
+    public boolean onCommand(CommandSender Sender, Command cmd, String label, String[] args) {
+        
+        if(cmd.getName().equals("Heal")) {
+           if (sender instanceof Player) {
+               Player player = (Player) sender;
+                
+               player.sendMessage("You Have Been Healed");
+            } else {
+                system.out.println("You Cannot Use This Command In Console")
+            }
+
+        }
+        
+        return false;
+    }
 
 }
 
